0d591c4b0d78a87aabd15b9581fee0ccce6295de
Ernest Shefer <shef-er@ya.ru>
2018-09-30 23:33:25 +0500

    google map fix

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,3 +1,24 @@
+// Google Map
+google.maps.visualRefresh = true;
+function initialize() {
+    var myLatlng = new google.maps.LatLng(56.8360658,60.5559682);
+    var mapOptions = {
+        zoom: 17,
+        center: myLatlng
+    };
+    var map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+    var contentString = '<div>SCHEF SPORTSWEAR</div>';
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'SCHEF SPORTSWEAR'
+    });
+    infowindow.open(map,marker);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
 
 // Gallery
 $('.gallery-images').slick({
